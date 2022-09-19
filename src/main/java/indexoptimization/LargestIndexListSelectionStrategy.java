ff753aa7fe17fd48088ef03a390709469e504f67
Artur Olszak <aolszak@gmail.com>
2022-09-19 13:05:13 +0200

    modified a comment

--- src/main/java/indexoptimization/LargestIndexListSelectionStrategy.java
+++ src/main/java/indexoptimization/LargestIndexListSelectionStrategy.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class LargestIndexListSelectionStrategy implements IndexListSelectionStrategy {
     @Override
     public List<Index> choseBestIndexes(List<List<Index>> indexCandidates) {
-        //chose the best set (smallest number of indexes)
+        //chose the best set (largest number of indexes)
         int maxSize = -1;
         List<Index> bestIndexList = null;
         for (List<Index> indexes : indexCandidates) {
